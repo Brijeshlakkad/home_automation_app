cbe3b3c313ccb82e6a7fb58927745cb5501ee40a
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-03-03 15:04:38 +0530

    SplashScreen

--- lib/app.dart
+++ lib/app.dart
@@ -4,10 +4,13 @@ import 'package:home_automation/colors.dart';
 import 'package:home_automation/login_signup/login.dart';
 import 'package:home_automation/login_signup/signup.dart';
 import 'package:home_automation/hardware.dart';
-class HomeAutomation extends StatefulWidget{
+import 'package:splashscreen/splashscreen.dart';
+
+class HomeAutomation extends StatefulWidget {
   HomeAutomationState createState() => HomeAutomationState();
 }
-class HomeAutomationState extends State<HomeAutomation>{
+
+class HomeAutomationState extends State<HomeAutomation> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +19,7 @@ class HomeAutomationState extends State<HomeAutomation>{
       theme: _kHomeAutomationTheme,
       initialRoute: '/',
       routes: {
-        '/': (context) => LoginScreen(),
+        '/': (context) => HomeAutomationSplashScreen(),
         '/login': (context) => LoginScreen(),
         '/signup': (context) => SignupScreen(),
         '/home': (context) => HomeScreen(),
@@ -25,6 +28,34 @@ class HomeAutomationState extends State<HomeAutomation>{
     );
   }
 }
+
+class HomeAutomationSplashScreen extends StatefulWidget {
+  @override
+  _HomeAutomationSplashScreenState createState() =>
+      new _HomeAutomationSplashScreenState();
+}
+
+class _HomeAutomationSplashScreenState
+    extends State<HomeAutomationSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 3,
+      loadingText: Text("Please wait"),
+      navigateAfterSeconds: new LoginScreen(),
+      title: new Text(
+        'Home Automation',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
+      ),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: () => print("Home Automation"),
+      loaderColor: Colors.white,
+    );
+  }
+}
+
 final ThemeData _kHomeAutomationTheme = _buildAppTheme();
 
 ThemeData _buildAppTheme() {
@@ -41,9 +72,7 @@ ThemeData _buildAppTheme() {
       buttonColor: kHAutoBlue100,
       textTheme: ButtonTextTheme.normal,
     ),
-    primaryIconTheme: base.iconTheme.copyWith(
-        color: kHAutoBlue900
-    ),
+    primaryIconTheme: base.iconTheme.copyWith(color: kHAutoBlue900),
     textTheme: _buildAppTextTheme(base.textTheme),
     primaryTextTheme: _buildAppTextTheme(base.primaryTextTheme),
     accentTextTheme: _buildAppTextTheme(base.accentTextTheme),
@@ -51,23 +80,23 @@ ThemeData _buildAppTheme() {
 }
 
 TextTheme _buildAppTextTheme(TextTheme base) {
-  return base.copyWith(
-    headline: base.headline.copyWith(
-      fontWeight: FontWeight.w500,
-    ),
-    title: base.title.copyWith(
-        fontSize: 18.0
-    ),
-    caption: base.caption.copyWith(
-      fontWeight: FontWeight.w400,
-      fontSize: 14.0,
-    ),
-    body2: base.body2.copyWith(
-      fontWeight: FontWeight.w500,
-      fontSize: 16.0,
-    ),
-  ).apply(
-    displayColor: kHAutoBlue900,
-    bodyColor: kHAutoBlue900,
-  );
+  return base
+      .copyWith(
+        headline: base.headline.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        title: base.title.copyWith(fontSize: 18.0),
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        body2: base.body2.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        displayColor: kHAutoBlue900,
+        bodyColor: kHAutoBlue900,
+      );
 }
